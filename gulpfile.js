585f2fb767ba5428d9238f5508f6e43b742575c9
Marcel Tinner <m1tinner@gmail.com>
2016-06-28 12:43:23 +0200

    remove node_modules copytask

--- gulpfile.js
+++ gulpfile.js
@@ -9,67 +9,58 @@ var gulp = require('gulp'),
 
 
 gulp.task('default', function (callback) {
-  runSequence(
-      'cleanDist',
-      ['transpiling','sass'],
-      ['copyApp', 'copyScripts', 'copyNodeModules'],
-      'connectDist',
-      callback
-  );
+    runSequence(
+        'cleanDist',
+        ['transpiling', 'sass'],
+        ['copyApp', 'copyScripts'],
+        'connectDist',
+        callback
+    );
 });
 
 gulp.task('cleanDist', function () {
-  return gulp.src('.dist', {read: false})
-      .pipe(clean());
+    return gulp.src('.dist', {read: false})
+        .pipe(clean());
 });
 
 gulp.task('transpiling', function () {
-  var tsResult= tsProject.src()
-      .pipe(sourcemaps.init())
-      .pipe(ts(tsProject));
+    var tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject));
 
-  return tsResult.js
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('.dist/webapp/app'));
+    return tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('.dist/webapp/app'));
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./src/main/webapp/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('.dist/webapp'));
+    return gulp.src('./src/main/webapp/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('.dist/webapp'));
 });
 
 gulp.task('connectDist', function () {
-  return connect.server({
-    root: ['.dist/webapp','node_modules'],
-    port: 9000
-  });
+    return connect.server({
+        root: ['.dist/webapp', 'node_modules'],
+        port: 9000
+    });
 });
 
 gulp.task('copyApp', function () {
-  return gulp.src([
+    return gulp.src([
         './src/main/webapp/**',
         '!./src/main/webapp/**/*.ts',
         '!./src/main/webapp/**/*.scss'
-      ], {base: './src/main/webapp'})
-      .pipe(gulp.dest('.dist/webapp'));
+    ], {base: './src/main/webapp'})
+        .pipe(gulp.dest('.dist/webapp'));
 });
 
 gulp.task('copyScripts', function () {
-  return gulp.src([
+    return gulp.src([
         'node_modules/core-js/client/shim.min.js',
         'node_modules/systemjs/dist/system.src.js',
         'node_modules/zone.js/dist/zone.js'
-      ])
-      .pipe(gulp.dest('.dist/webapp/app/scripts/vendor'));
-});
-
-gulp.task('copyNodeModules', function () {
-  return gulp.src([
-        'node_modules/@angular/*/bundles/*.js',
-        'node_modules/rxjs/**',
-        'node_modules/angular2-in-memory-web-api/**'
-      ], {base: './node_modules/*'})
-      .pipe(gulp.dest('.dist/webapp/app/scripts/vendor/*'));
+    ])
+        .pipe(gulp.dest('.dist/webapp/app/scripts/vendor'));
 });
 
