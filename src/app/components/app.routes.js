b5a4190f5efe36a955b798910530674b19e13c5d
Marcel Tinner <m1tinner@gmail.com>
2016-07-24 09:16:50 +0200

    remove redundant js files

--- src/app/components/app.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-var heroes_routes_1 = require("./hero/heroes/heroes.routes");
-var dashboard_routes_1 = require("./hero/dashboard/dashboard.routes");
-var hero_detail_routes_1 = require("./hero/heroDetail/hero-detail.routes");
-exports.routes = dashboard_routes_1.DashboardRoutes.concat(heroes_routes_1.HeroesRoutes, hero_detail_routes_1.HeroDetailRoutes);
-exports.APP_ROUTER_PROVIDERS = [
-    router_1.provideRouter(exports.routes)
-];
--- src/app/components/hero/heroDetail/hero-detail.routes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var hero_detail_component_1 = require('../heroDetail/hero-detail.component');
-exports.HeroDetailRoutes = [
-    { path: 'hero/:id', component: hero_detail_component_1.HeroDetailComponent }
-];
