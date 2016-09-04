98cd4c6140093983aea4587e568a62e689c6c9ee
Tobias Büchel <tobi.buechel@gmail.com>
2016-09-04 13:36:01 +0200

    Implemented automatic jwt renewal on protected resources

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,24 +1,24 @@
 'use strict';
 
 let express = require('express'),
-  router = express.Router(),
-  authService = require('../services/authService');
+    router = express.Router(),
+    authService = require('../services/authService');
 
 router.get('/token', function (req, res) {
-  let token = authService.signIn(
-    req.query.username,
-    req.query.password);
-  if (token) {
-    res.setHeader('X-Auth-Token', token);
-    res.status(200).send();
-  }
-  res.status(401).send();
+    let token = authService.signIn(
+        req.query.username,
+        req.query.password);
+    if (token) {
+        res.setHeader('X-Auth-Token', token);
+        res.status(200).send();
+    }
+    res.status(401).send();
 });
 
 router.get('/test',
-  authService.protect('admin'),
-  function (req, res) {
-    res.status(200).send(req.user);
-  });
+    authService.protect('admin'),
+    function (req, res) {
+        res.status(200).send(req.user);
+    });
 
 module.exports = router;
--- src/backend/services/authService.js
+++ src/backend/services/authService.js
@@ -1,49 +1,61 @@
 'use strict';
 
-let expressJwt = require('express-jwt'),
-  jwt = require('jsonwebtoken'),
-  guard = require('express-jwt-permissions')({permissionsProperty: 'roles'});
+let jwt = require('jsonwebtoken'),
+    expressJwt = require('express-jwt'),
+    guard = require('express-jwt-permissions')({permissionsProperty: 'roles'});
 
 let authService = (function () {
-  const SECRET = 'c4ntT0uchTh1s';
-  const users = [ //todo: get from DB
-    {
-      username: 'admin',
-      password: 'pwd',
-      roles: ['user', 'admin']
-    },
-    {
-      username: 'appUser',
-      password: 'pwd',
-      roles: ['user']
-    }
-  ];
+    const SECRET = 'c4ntT0uchTh1s';
+    const users = [ //todo: get from DB
+        {
+            username: 'admin',
+            password: 'pwd',
+            roles: ['user', 'admin']
+        },
+        {
+            username: 'appUser',
+            password: 'pwd',
+            roles: ['user']
+        }
+    ];
 
-  return {
-    protect: protect,
-    signIn: signIn
-  };
+    return {
+        protect: protect,
+        signIn: signIn
+    };
 
-  function protect(guardedRoles) {
-    return function (req, res, next) {
-      expressJwt({secret: SECRET})(req, res, function () {
-        guard.check(guardedRoles)(req, res, next);
-      });
+    function protect(guardedRoles) {
+        return function (req, res, next) {
+            expressJwt({secret: SECRET})(req, res, function () {
+                if(!req.user){
+                    res.status(401).send();
+                }
+                let matchedUsers = users.filter(user => user.username === req.user.username);
+                if (matchedUsers && matchedUsers.length > 0) {
+                    let signedInUser = Object.assign({}, matchedUsers[0]);
+                    delete signedInUser.password;
+                    var token = jwt.sign(signedInUser, SECRET);
+                    res.setHeader('X-Auth-Token', token);
+                    guard.check(guardedRoles)(req, res, next);
+                }else {
+                    res.status(500).send(`username ${req.username} not found`);
+                }
+            });
+        }
     }
-  }
 
-  function signIn(username, password) {
-    let matchedUsers = users.filter(user =>
-      user.username === username &&
-      user.password === password
-    );
-    if (matchedUsers && matchedUsers.length > 0) {
-      let signedInUser = Object.assign({}, matchedUsers[0]);
-      delete signedInUser.password;
-      return jwt.sign(signedInUser, SECRET);
+    function signIn(username, password) {
+        let matchedUsers = users.filter(user =>
+            user.username === username &&
+            user.password === password
+        );
+        if (matchedUsers && matchedUsers.length > 0) {
+            let signedInUser = Object.assign({}, matchedUsers[0]);
+            delete signedInUser.password;
+            return jwt.sign(signedInUser, SECRET);
+        }
+        return null;
     }
-    return null;
-  }
 })();
 
 module.exports = authService;
