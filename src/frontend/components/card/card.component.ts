82dc0dd8dc788e35a89a36d7ae7170810a0e2d10
Marcel Tinner <m1tinner@gmail.com>
2016-09-11 22:03:22 +0200

    add basic chip stuff

--- src/frontend/components/card/card.component.ts
+++ src/frontend/components/card/card.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Group} from "../../models/Group";
+import {ChipComponent} from "../chip/chip.component";
 
 
 @Component({
     selector: 'card',
     templateUrl: 'frontend/components/card/card.component.html',
     styleUrls: ['frontend/components/card/card.component.css'],
+    directives: [ChipComponent]
 })
 export class CardComponent {
     @Input()
---
+++ src/frontend/components/chip/chip.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+import {Group} from "../../models/Group";
+
+
+@Component({
+    selector: 'chip',
+    templateUrl: 'frontend/components/chip/chip.component.html',
+    styleUrls: ['frontend/components/chip/chip.component.css'],
+})
+export class ChipComponent {
+
+}
