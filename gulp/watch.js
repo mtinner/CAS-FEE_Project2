fad43974860c0e9d8afdc4404d451c13bb6f91d9
Tobias Büchel <tobi.buechel@gmail.com>
2016-09-14 13:20:22 +0200

    Added watch for scss compiling

--- gulp/watch.js
+++ gulp/watch.js
@@ -3,11 +3,23 @@ module.exports = function (gulp, data, util, taskName) {
     var gulp = require('gulp'),
         watch = require('gulp-watch'),
         print = require('gulp-print'),
-        runSequence = require('run-sequence');
+        runSequence = require('run-sequence'); 
 
     gulp.task(taskName + ':Transpiling', function () {
         return watch(data.path.frontend + '**/*.ts', function () {
             runSequence('transpiling:Dist');
-        }).pipe(print());
+        }).pipe(_printChanges());
     });
+
+    gulp.task(taskName + ':Sass', function () {
+        return watch(data.path.frontend + '**/*.scss', function () {
+            runSequence('sass:Dist');
+        }).pipe(_printChanges());
+    });
+
+    function _printChanges() {
+        return print(function (filepath) {
+            return 'Changed: ' + filepath;
+        });
+    }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('Default', function (callback) {
         ['transpiling:Dist', 'sass:Dist'],
         ['copy:App'],
         ['serve:Dist'],
-        ['watch:Transpiling'],
+        ['watch:Transpiling', 'watch:Sass'],
         callback
     );
 });
