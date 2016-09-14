98d1324f5d17612903cbff2ebf1beffd09d0d084
Tobias Büchel <tobi.buechel@gmail.com>
2016-09-14 17:07:30 +0200

    Implemented linter for backend (eslint)

--- gulp/lint.js
+++ gulp/lint.js
@@ -1,14 +1,21 @@
 module.exports = function (gulp, data, util, taskName) {
 
-    var tslint = require('gulp-tslint');
+    var tslint = require('gulp-tslint'),
+        eslint = require('gulp-eslint');
 
     gulp.task(taskName + ':Ts', () =>
         gulp.src(data.path.frontend + '**/*.ts')
             .pipe(tslint({
-                formatter: "verbose"
+                formatter: 'verbose'
             }))
             .pipe(tslint.report({
                 emitError: false
             }))
+    );
+
+    gulp.task(taskName + ':Es', () =>
+        gulp.src(data.path.backend + '**/*.js')
+            .pipe(eslint())
+            .pipe(eslint.format())
     );
 };
