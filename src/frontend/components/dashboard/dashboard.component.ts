ff5f24495a95f43e5519f7aa587bd19f8dfc5b92
mtinner <m1tinner@gmail.com>
2016-09-16 15:42:50 +0200

    onHashChange draft

--- src/frontend/components/dashboard/dashboard.component.ts
+++ src/frontend/components/dashboard/dashboard.component.ts
@@ -1,28 +1,26 @@
 import {Component} from '@angular/core';
 import {DashboardService} from "./dashboard.service";
+import {Router} from '@angular/router';
 
 
 @Component({
-    selector: 'my-dashboard',
-    templateUrl: 'frontend/components/dashboard/dashboard.component.html',
-    styleUrls: ['frontend/components/dashboard/dashboard.component.css'],
-    providers: [DashboardService]
+	selector: 'my-dashboard',
+	templateUrl: 'frontend/components/dashboard/dashboard.component.html',
+	styleUrls: ['frontend/components/dashboard/dashboard.component.css'],
+	providers: [DashboardService]
 })
 export class DashboardComponent {
 
-    private showNavigation = true;
-    private menuItems: string[] = [];
-    private selectedIndex: number = 0;
+	private showNavigation = true;
+	private menuItems = [];
+	private selectedIndex: number = 0;
 
-    constructor(private dashboardService: DashboardService) {
-        this.menuItems = dashboardService.getMenuItems();
-    }
+	constructor(private dashboardService: DashboardService, private router: Router) {
+		this.menuItems = dashboardService.getMenuItems();
+		router.events.subscribe((val) => this.selectedIndex = this.menuItems.findIndex(menuItem=>val.url === menuItem.route) || 0);
+	}
 
-    toggleNavigation() {
-        this.showNavigation = !this.showNavigation;
-    }
-
-    setSelectedIndex(index) {
-        this.selectedIndex = index;
-    }
+	toggleNavigation() {
+		this.showNavigation = !this.showNavigation;
+	}
 }
--- src/frontend/components/dashboard/dashboard.service.ts
+++ src/frontend/components/dashboard/dashboard.service.ts
@@ -3,14 +3,14 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class DashboardService {
 
-    private menuItems = [
-        {name: 'Shopping List', icon: 'shopping-list', route: 'shopping-list'},
-        {name: 'Cost Management', icon: 'cost-management', route: 'cost-management'},
-        {name: 'Auth', icon: 'settings', route: 'login'},
-        {name: 'Settings', icon: 'settings'}
-    ];
+	private menuItems = [
+		{name: 'Shopping List', icon: 'shopping-list', route: 'shopping-list'},
+		{name: 'Cost Management', icon: 'cost-management', route: 'cost-management'},
+		{name: 'Auth', icon: 'settings', route: 'login'},
+		{name: 'Settings', icon: 'settings', route: 'settings'}
+	];
 
-    getMenuItems(): Array<any> {
-        return this.menuItems;
-    }
+	getMenuItems(): Array<any> {
+		return this.menuItems;
+	}
 }
