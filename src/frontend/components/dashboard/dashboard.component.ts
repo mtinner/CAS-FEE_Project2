30006eb5ac1e89326b29675ba1a9d0a1e12dde3b
Marcel Tinner <m1tinner@gmail.com>
2016-09-16 19:25:30 +0200

    on hash change route finish

--- src/frontend/components/dashboard/dashboard.component.ts
+++ src/frontend/components/dashboard/dashboard.component.ts
@@ -1,26 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DashboardService} from "./dashboard.service";
 import {Router} from '@angular/router';
 
 
 @Component({
-	selector: 'my-dashboard',
-	templateUrl: 'frontend/components/dashboard/dashboard.component.html',
-	styleUrls: ['frontend/components/dashboard/dashboard.component.css'],
-	providers: [DashboardService]
+    selector: 'my-dashboard',
+    templateUrl: 'frontend/components/dashboard/dashboard.component.html',
+    styleUrls: ['frontend/components/dashboard/dashboard.component.css'],
+    providers: [DashboardService]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
-	private showNavigation = true;
-	private menuItems = [];
-	private selectedIndex: number = 0;
+    private showNavigation = true;
+    private menuItems: Array<any> = [];
+    private selectedIndex: number = 0;
 
-	constructor(private dashboardService: DashboardService, private router: Router) {
-		this.menuItems = dashboardService.getMenuItems();
-		router.events.subscribe((val) => this.selectedIndex = this.menuItems.findIndex(menuItem=>val.url === menuItem.route) || 0);
-	}
+    constructor(private dashboardService: DashboardService, private router: Router) {
+        this.menuItems = dashboardService.getMenuItems();
+    }
 
-	toggleNavigation() {
-		this.showNavigation = !this.showNavigation;
-	}
+    toggleNavigation() {
+        this.showNavigation = !this.showNavigation;
+    }
+
+    ngOnInit(): void {
+        this.determineActiveRoute();
+    }
+
+    determineActiveRoute() {
+        this.router.events.subscribe((val) => {
+            let index = this.menuItems.findIndex(menuItem=>
+                val.url === menuItem.route
+            );
+            this.selectedIndex = index < 0 ? 0 : index;
+        });
+    }
 }
