a1e8d77c9d8b771448874f13b0d75ee6ded95022
Marcel Tinner <marcel.tinner@hsr.ch>
2016-10-06 12:09:22 +0200

    remove test

--- src/backend/routes/articleRoutes.mocha.js
+++ src/backend/routes/articleRoutes.mocha.js
@@ -53,63 +53,6 @@ describe('/article', ()=> {
         });
     });
 
-    describe('/PUT', ()=> {
-
-        it('respond with 200', (done)=> {
-            request.put(url, {name: 'Gurken', group: 'Alle'}, (error, response)=> {
-                expect(response.statusCode).to.equal(200);
-                done();
-            });
-        });
-
-        it('respond with new object', (done)=> {
-            request.put({
-                url: url,
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify({name: 'Gurken', group: 'Alle'})
-            }, (error, response, body)=> {
-                let article = JSON.parse(body);
-                expect(response.statusCode).to.equal(200);
-                expect(article).to.be.a('object');
-                expect(article.id).to.be.a('number');
-                expect(article.name).to.be.a('string');
-                expect(article.group).to.be.a('string');
-                done();
-            });
-        });
-
-        it('respond with updated object', (done)=> {
-            request.put({
-                url: url,
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify({name: 'Gurken', group: 'Alle'})
-            }, (error, response, body) => {
-                let id = JSON.parse(body).id;
-
-                request.put({
-                    url: `${url}/${id}`,
-                    headers: {
-                        'content-type': 'application/json'
-                    },
-                    body: JSON.stringify({name: 'Gurken', group: 'Früchte'})
-                }, (error, response, body) => {
-                    let article = JSON.parse(body);
-                    expect(response.statusCode).to.equal(200);
-                    expect(article).to.be.a('object');
-                    expect(article.id).to.be.a('number');
-                    expect(article.id).to.equal(id);
-                    expect(article.name).to.be.a('string');
-                    expect(article.group).to.be.a('string');
-                    done();
-                });
-            });
-        });
-    });
-
     describe('/DELETE', ()=> {
 
         it('respond with 404', (done)=> {
