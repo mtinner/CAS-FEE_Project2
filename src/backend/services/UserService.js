d75bb4fcded450cff5f5091b58e5accd0cb19fe4
Marcel Tinner <marcel.tinner@hsr.ch>
2016-10-06 13:35:23 +0200

    add basic services

---
+++ src/backend/services/UserService.js
@@ -0,0 +1,25 @@
+'use strict';
+let NedbRepo = require ('./NedbRepo');
+
+class UserService{
+    constructor(){
+        this.nedbRepo = new NedbRepo('user');
+    }
+
+    get(id) {
+        //TODO get userGroup and so on
+        return this.nedbRepo.get(id);
+    }
+
+    add(newDoc) {
+        //TODO get userGroup and so on
+        return this.nedbRepo.add(newDoc);
+    }
+
+    remove(id) {
+        //TODO get userGroup and so on
+        return this.nedbRepo.remove(id);
+    }
+}
+
+module.exports = UserService;
