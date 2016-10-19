52ff139d71d04103389559d814bfb87bf0512a6d
Marcel Tinner <m1tinner@gmail.com>
2016-10-19 07:23:40 +0200

    add modal

--- src/frontend/components/pages/content/+settings/group-members.component.ts
+++ src/frontend/components/pages/content/+settings/group-members.component.ts
@@ -11,15 +11,31 @@ import {Params, ActivatedRoute} from '@angular/router';
     styleUrls: ['group-members.component.css']
 })
 export class GroupMembersComponent implements OnInit, OnDestroy {
+    private inputField: Object = {
+        invitedEmail: {placeholder: 'Email', type: 'mail'}
+    };
+
+
+    private invitedEmail: string = '';
+    private groupId: string = '';
+    private showModal: boolean = false;
 
     constructor(private headerService: HeaderService, private groupService: GroupService, private route: ActivatedRoute) {
         this.headerService.headerConfig = new HeaderConfig('Group Members', HeaderStyle.Settings, HeaderIcon.ArrowLeft, this.groupService.goToGroups, HeaderIcon.Leave);
     }
 
+    setInvitedEmail(value: string) {
+        this.invitedEmail = value.trim();
+    }
+
+    setModalVisibility(value: boolean) {
+        this.showModal = value;
+    }
+
     ngOnInit(): void {
         this.route.params.forEach((params: Params) => {
-            let id = params['id'];
-            this.groupService.getMembers(id);
+            this.groupId= params['id'];
+            this.groupService.getMembers(this.groupId);
         });
     }
 
