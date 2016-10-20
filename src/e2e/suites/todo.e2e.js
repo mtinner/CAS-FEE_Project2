8bfcd2fc040c1ad18fdf18269eed8fd0e652f658
Marcel Tinner <m1tinner@gmail.com>
2016-10-20 13:06:58 +0200

    reset db after each test

--- src/e2e/suites/todo.e2e.js
+++ src/e2e/suites/todo.e2e.js
@@ -1,14 +1,21 @@
 var dashboardPage = require('../pageobjects/dashboard');
+var cleanup = require('../cleanup');
+
 
 describe('angularjs homepage todo list', function () {
+
+    afterEach(() => {
+        cleanup.removeDB();
+    });
+
     it('should add a todo', function () {
-       browser.get(dashboardPage.getUrl());
-       /* dashboardPage.heroNames().first().getText().then(function (text) {
-            expect(text).toEqual('Narco');
-        });
-        expect(dashboardPage.title().getText().then(function (text) {
-            expect(text).toEqual('Top Heroes');
-        }));
-        expect(dashboardPage.heroes().count()).toEqual(4);*/
+        browser.get(dashboardPage.getUrl());
+        /* dashboardPage.heroNames().first().getText().then(function (text) {
+         expect(text).toEqual('Narco');
+         });
+         expect(dashboardPage.title().getText().then(function (text) {
+         expect(text).toEqual('Top Heroes');
+         }));
+         expect(dashboardPage.heroes().count()).toEqual(4);*/
     });
 });
