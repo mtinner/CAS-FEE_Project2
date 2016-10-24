568b807e571bfeb7fc79e1063f2f84653c8c40ea
Marcel Tinner <m1tinner@gmail.com>
2016-10-24 19:13:03 +0200

    cleanup Articlerouting

--- src/backend/routes/apiRoutes.js
+++ src/backend/routes/apiRoutes.js
@@ -2,12 +2,12 @@
 let express = require('express'),
     router = express.Router();
 
-let shoppingListRoutes = require('./shoppingListRoutes');
+let articleRoutes = require('./articleRoutes');
 let authRoutes = require('./authRoutes');
 let registerRoutes = require('./registerRoutes');
 let groupRoutes = require('./groupRoutes');
 
-router.use('/shoppingList', shoppingListRoutes);
+router.use('/articles', articleRoutes);
 router.use('/auth', authRoutes);
 router.use('/groups', groupRoutes);
 router.use('/register', registerRoutes);
--- src/backend/routes/articleRoutes.js
+++ src/backend/routes/articleRoutes.js
@@ -2,13 +2,14 @@
 
 let express = require('express'),
     router = express.Router(),
-    shoppingListService = require('../services/shoppingListService'),
-    authService = require('../services/authService');
+    ArticleManager = require('../manager/ArticleManager'),
+    authService = require('../services/authService'),
+    articleManager = new ArticleManager();
 
 router.get('/',
     authService.protect('user'),
     function (req, res) {
-        shoppingListService.getArticles(req.user)
+        articleManager.getAll(req.user)
             .then(articles => res.status(200).send(articles))
             .catch(() => res.status(404).send());
     });
@@ -16,24 +17,20 @@ router.get('/',
 router.post('/',
     authService.protect('user'),
     function (req, res) {
-        shoppingListService.addArticle(req.body, req.user)
+        articleManager.add(req.body, req.user)
             .then(article => res.status(201).send(article));
     });
 
 router.delete('/:id',
     authService.protect('user'),
     function (req, res) {
-        try {
-            shoppingListService.deleteArticle(req.params.id)
-                .then(article => res.status(200).send(article));
-        }
-        catch (e) {
-            res.status(404).send();
-        }
+        articleManager.remove(req.params.id)
+            .then(article => res.status(200).send(article))
+            .catch(() => res.status(404).send());
     });
 
 router.get('/groups', function (req, res) {
-    res.status(200).send(shoppingListService.getArticleGroups());
+    res.status(200).send(articleManager.getGroups());
 });
 
 module.exports = router;
