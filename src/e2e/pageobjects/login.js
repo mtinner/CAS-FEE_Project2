5327921f5bcf8871b9fefeed4b88d75d9a9644ef
Marcel Tinner <m1tinner@gmail.com>
2016-11-04 18:59:16 +0100

    some fixes

--- src/e2e/pageobjects/login.js
+++ src/e2e/pageobjects/login.js
@@ -1,13 +1,13 @@
 module.exports = (function () {
 
-    var inputEmail = element.all(by.css('[e2e-id="email"] input'));
-    var inputPassword = element.all(by.css('[e2e-id="password"] input'));
-    var loginButton = element.all(by.css('[e2e-id="loginBtn"]'));
+    var inputEmail = element(by.css('[e2e-id="email"] input'));
+    var inputPassword = element(by.css('[e2e-id="password"] input'));
+    var loginButton = element(by.css('[e2e-id="loginBtn"]'));
 
     return {
         loginButton: () => loginButton,
         inputEmail: () => inputEmail,
         inputPassword: () => inputPassword,
         getUrl: () => 'http://localhost:8080/login'
-    }
+    };
 })();
--- src/e2e/suites/login.e2e.js
+++ src/e2e/suites/login.e2e.js
@@ -12,7 +12,7 @@ describe('login', function () {
     it('should login', function () {
         browser.get(loginPage.getUrl());
         var inputEmail = loginPage.inputEmail().click()
-            .then(()=> loginPage.inputEmail().sendKeys('appUser'));
+            .then(()=> loginPage.inputEmail().sendKeys('appUser@admin.ch'));
         var inputPassword = loginPage.inputPassword().click()
             .then(()=> loginPage.inputPassword().sendKeys('pwd'));
 
