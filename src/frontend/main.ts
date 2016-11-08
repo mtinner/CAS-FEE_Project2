c5df502305a40f5a2ce1058719939296f9d60004
mtinner <m1tinner@gmail.com>
2016-11-08 12:13:05 +0100

    remove enabledProdMode log

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,14 +1,16 @@
 // removeIf(development)
-import {AppModuleNgFactory} from '../.aot/src/.tmpProd/components/app.module.ngfactory';
-import {platformBrowser} from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../.aot/src/.tmpProd/components/app.module.ngfactory';
+import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 // endRemoveIf(development)
 
 
 // removeIf(production)
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './components/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './components/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 // endRemoveIf(production)
