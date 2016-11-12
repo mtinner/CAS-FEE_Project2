6ac0b9325b669fcfe5b646b4e8af6ea58a30d5d7
Marcel Tinner <m1tinner@gmail.com>
2016-11-12 10:14:22 +0100

    shoppinglist

--- src/frontend/components/pages/content/+shoppingList/shopping-list.component.ts
+++ src/frontend/components/pages/content/+shoppingList/shopping-list.component.ts
@@ -18,18 +18,18 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     }
 
     deleteArticle(article: Article) {
-        this.shoppingListService.deleteArticle(article.id);
+        this.shoppingListService.deleteArticle(article.id).subscribe();
     }
 
     addArticle(item: string, articleGroup: ArticleGroup) {
-        this.shoppingListService.addArticle({name: item, articleGroup: articleGroup.id});
+        this.shoppingListService.addArticle({name: item, articleGroup: articleGroup.id}).subscribe();
     }
 
     ngOnInit(): void {
         this.headerService.headerConfig = new HeaderConfig('Shopping List', HeaderStyle.ShoppingList);
-        this.shoppingListService.fetchArticleGroups();
+        this.shoppingListService.fetchArticleGroups().subscribe();
 
-        this.shoppingListService.fetchArticles();
+        this.shoppingListService.fetchArticles().subscribe();
     }
 
     ngOnDestroy(): void {
