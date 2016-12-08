dbbe400d2461376fa939dcd4545a585756485f2f
Marcel Tinner <m1tinner@gmail.com>
2016-12-08 07:28:56 +0100

    karma gulp job

---
+++ gulp/karma.js
@@ -0,0 +1,11 @@
+module.exports = function(gulp, data, util, taskName) {
+    var Server = require('karma').Server,
+        path = require('path');
+
+    gulp.task(taskName + ':Frontend', function(done) {
+        new Server({
+            configFile: path.join(__dirname, '/../', 'karma.conf.js'),
+            singleRun: true
+        }, done).start();
+    });
+};
