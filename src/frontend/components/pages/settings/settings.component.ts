127eb9d042dd54e5e7ca01a3edac97ec4f9778f7
Tobias Büchel <tobi.buechel@gmail.com>
2016-12-17 19:26:32 +0100

    Removed dummy settings entry

--- src/frontend/components/pages/settings/settings.component.ts
+++ src/frontend/components/pages/settings/settings.component.ts
@@ -11,9 +11,7 @@ import {HeaderConfig} from '../../../models/HeaderConfig';
 })
 export class SettingsComponent implements OnDestroy {
     public settings: Setting[] = [
-        new Setting('/groups', 'Group Membership', `Manage with whom you're sharing shopping list & expenses`),
-        new Setting('.', 'Article Groups', 'Set how individual articles should be grouped'),
-        new Setting('.', 'History')
+        new Setting('/groups', 'Group Membership', `Manage with whom you're sharing shopping list & expenses`)
     ];
 
     constructor(private headerService: HeaderService) {
